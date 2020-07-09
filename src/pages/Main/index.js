e112df07d1c1f1df2bf133725ac8e96007732ac2
Evaldo <evaldocz18@gmail.com>
2020-07-09 20:43:21 -0300

    Estilos globais

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -3,12 +3,7 @@ import { Title } from './styles';
 // import { Container } from './styles';
 
 function Main() {
-  return (
-    <Title error>
-      Main
-      <small>menor</small>
-    </Title>
-  );
+  return <Title>Hello World</Title>;
 }
 
 export default Main;
