82d71af313c0b75fd93efe4cc2088436c380d051
Evaldo <evaldocz18@gmail.com>
2020-07-09 20:29:17 -0300

    Styled components

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,9 +1,14 @@
 import React from 'react';
-
+import { Title } from './styles';
 // import { Container } from './styles';
 
 function Main() {
-  return <h1>Main</h1>;
+  return (
+    <Title error>
+      Main
+      <small>menor</small>
+    </Title>
+  );
 }
 
 export default Main;
