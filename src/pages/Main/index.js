e7b0ccef717cea3353ed99181327939797962466
Evaldo <evaldocz18@gmail.com>
2020-07-10 10:20:58 -0300

    Estilizando página Main

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,9 +1,25 @@
 import React from 'react';
-import { Title } from './styles';
+import { FaGithubAlt, FaPlus } from 'react-icons/fa';
+import { Container, Form, SubmitButton } from './styles';
 // import { Container } from './styles';
 
 function Main() {
-  return <Title>Hello World</Title>;
+  return (
+    <Container>
+      <h1>
+        <FaGithubAlt />
+        Repositórios
+      </h1>
+
+      <Form onSubmit={() => {}}>
+        <input type="text" placeholder="Adicionar remetente" />
+
+        <SubmitButton disable>
+          <FaPlus color="#FFF" size={14} />
+        </SubmitButton>
+      </Form>
+    </Container>
+  );
 }
 
 export default Main;
